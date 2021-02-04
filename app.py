aa8958b98dc6354e2eb0708faf23ed806144d8bd
Taban Yolo <tabanyolo@yahoo.com>
2021-02-04 10:31:50 -0800

    Updating app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from Bankingbot import category
 from Bankingbot import BankBot
 import pandas as pd
 
-mytrans = BankBot("megaskidboy", "Salvation1")
-mytrans.get_account_name("Miscellaneous - 8596")
-mytrans.get_date("01/13/2021")
+mytrans = BankBot("YOUR LOGIN ID", "YOUR PASSWORD")
+mytrans.get_account_name("BANK ACCOUNT NAME")
+mytrans.get_date("DATE")
 mytrans.get_transactions()
 
 #print(date_price)
